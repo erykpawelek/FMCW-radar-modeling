b321f8c80c2c6a509201ac2ac26ab78ac4c857a6
Eryk Pawełek <panzereryk03@gmail.com>
2025-11-15 20:59:58 +0100

    FFT and displaying
    Comparison of tx, rx and beat signal. FFT transform part.

--- FMCW_Radar.m
+++ FMCW_Radar.m
@@ -59,15 +59,71 @@ else
     doppler_shift_signal = exp(1j * 2 * pi * f_d * t); % Generationg doppler shift signal
     noise = 0.5 * (randn(size(t)) + 1j * randn(size(t))); % Generating complex gausian noise 
     signal_rx = (exp(1j * phase_rx) .* doppler_shift_signal) + noise; %Return signal with noise and phase shift
+    % We multiply signals to make use of exponent adding properiety of a
+    % signals
     disp(['Travel time: ', num2str(tau), ' s']);
     disp(['Doppler freqency: ', num2str(f_d/1e6), ' MHz']);
+    
    % ---- Beat signal ----
    
-    beat_signal = signal_rx .* conj(signal_tx);
+    beat_signal = signal_rx .* conj(signal_tx); % Generating beat signal
+    % Beat signal is generated using Complex Conjugate function, by that we
+    % ramowe our tx component of recived signal (we subtract it)
+
+    % ---- printing results ----
     figure;
     plot(t, real(beat_signal));
     title('Beat Signal');
     xlabel('Time (s)');
     ylabel('Amplitude (Re)');
     grid on;
+    figure;
+    subplot(3,1,1);
+    plot(t(1:50000), real(signal_tx(1:50000)),'-g');
+    xlabel('Time (s)');
+    ylabel('Amplitude (Re)');
+    title('Transmitted signal');
+    subplot(3,1,2);
+    t_start = round(tau*fs)+1;
+    plot(t(t_start:50000), real(signal_rx(t_start:50000)),'-r');
+    xlabel('Time (s)');
+    ylabel('Amplitude (Re)');
+    title('Reived signal');
+    subplot(3,1,3);
+    plot(t(t_start:50000), real(beat_signal(t_start:50000)),'-b');
+    title('Beat signal');
+    xlabel('Time (s)');
+    ylabel('Amplitude (Re)');
+    grid on;
+
+    % ---- Fast Fourier Transform
+
+    beat_signal_cleaned = beat_signal(t_start:end); % Cutting of first samples (before received signal arivall)
+    N_fft = 2^nextpow2(length(beat_signal_cleaned)); % Optymalizing fft transform function
+    fft_ = fft(beat_signal_cleaned, N_fft); % Fast Fourier transform
+    fft_shifted = fftshift(fft_); % Shifting order
+    f_axis = fs * (-N_fft/2 : N_fft/2 - 1) / N_fft; % Creating x axis to print fft
+    
+
+    figure;
+    subplot(2,1,1)
+    plot(f_axis / 1e6, abs(fft_shifted)); 
+    title('FFT Spectrum of the Beat Signal');
+    xlabel('Frequency (MHz)');
+    ylabel('|fft(beat(signal)|');
+    grid on;
+    subplot(2,1,2)
+    plot(f_axis / 1e6, abs(fft_shifted)); 
+    title('FFT Spectrum of the Beat Signal');
+    xlabel('Frequency (MHz)');
+    ylabel('|fft(beat(signal)|');
+
+    [max_amplitude, idx_peak] = max(abs(fft_shifted));
+    f_peak_hz = f_axis(idx_peak); 
+    
+    % Peak zooming
+    zoom_range_hz = max(10e6, abs(f_peak_hz * 0.5)); 
+    xlim_min = (f_peak_hz - zoom_range_hz) / 1e6;
+    xlim_max = (f_peak_hz + zoom_range_hz) / 1e6;
+    xlim([max(xlim_min, f_axis(1)/1e6), min(xlim_max, f_axis(end)/1e6)]);
 end
